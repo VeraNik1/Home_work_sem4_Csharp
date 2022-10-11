a467016712768dadd43d60fcf36390f0674d3421
Vera <solnce288888@gmail.com>
2022-10-11 22:14:37 +0300

    num 29

--- Program.cs
+++ Program.cs
@@ -40,7 +40,42 @@ int SumDigits(int number){
 }
 
 /*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
 6, 1, 33 -> [6, 1, 33]*/
+
+
+//рандомные элементы в диапазоне от X до Y (количество элементов массива задает пользователь)
+
+Console.WriteLine("Ввeдите размер массива: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввeдите минимальное значение элемента массива: ");
+int min = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Ввeдите максимальное значение элемента массива: ");
+int max = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"[{String.Join(", ", ArrRand2(N, min, max))}]");
+
+int[] ArrRand2(int size, int low, int ceil){
+    int[] result = new int[size];
+
+    for(int i = 0; i < result.Length; i++){
+        result[i] = new Random().Next(low, ceil + 1);
+    }
+return result;
+}
+
+
+//количество элементов и элементы задает пользователь
+
+Console.WriteLine("Ввeдите размер массива: ");
+int len = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"[{String.Join(", ", ArrUser(len))}]");
+
+int[] ArrUser(int size){
+    int[] result = new int[size];
+
+    for(int i = 0; i < result.Length; i++){
+        Console.WriteLine($"Введите {i + 1}-й элемент массива: ");
+        result[i] = int.Parse(Console.ReadLine()!);
+    }
+return result;
+}
